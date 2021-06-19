4ed7c7ca77088cb38e090bb6563079ee5a1faec6
openehe20040408 <625529334@qq.com>
2021-06-19 15:25:56 +0800

    selenium first learn

---
+++ src/spider/selenium/test1.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+if __name__ == '__main__':	
+    driver=webdriver.Edge(r'C:\Users\william\AppData\Local\Programs\Python\Python38\Scripts\msedgedriver.exe')
+    driver.maximize_window()
+    driver.get('https://www.qingpufdfz.cn/')
+    
